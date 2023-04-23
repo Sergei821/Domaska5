aada0388aa7d6d204682bd54dbc81a03bdf1939a
Sergei821 <ps16111982@gmail.com>
2023-04-23 20:34:48 +0300

    zad1

---
+++ Task1/Program.cs
@@ -0,0 +1,43 @@
+﻿//Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+
+int[] CreateAndFillingArray(int size, int leftRangeValue, int rightRangeValue)
+{
+    int[] array = new int[size];
+
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(leftRangeValue, rightRangeValue);
+    }
+    return array;
+}
+
+void PrintArrayInConsole(int[] array)
+{
+    Console.WriteLine(string.Join(",", array));
+}
+
+int CountingTheNumberOfEvenNumbersInAnArray(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]%2 == 0)
+        {
+           result++;
+        }
+    }
+    return result;
+}
+
+int size = 12;
+
+int[] array = CreateAndFillingArray(size, 100, 1000);
+
+PrintArrayInConsole(array);
+
+System.Console.WriteLine($"Количество четных в массиве = {CountingTheNumberOfEvenNumbersInAnArray(array)}");
+
